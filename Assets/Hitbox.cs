91eb44b53482c9efdffdcc9a21f41f1320817fd5
MekaNeckd <cpullini@nyit.edu>
2024-04-06 17:14:56 -0400

    Swathi progress
    crawl animation work

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    public Collider2D currentHitbox; // Assign the initial hitbox in the inspector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeHitbox(GameObject newHitbox)
+    {
+        if (currentHitbox != null)
+        {
+            currentHitbox.enabled = false; // Disable the current hitbox
+        }
+
+        currentHitbox = newHitbox.GetComponent<Collider2D>();
+        if (currentHitbox != null)
+        {
+            currentHitbox.enabled = true; // Enable the new hitbox
+        }
+    }
+}
